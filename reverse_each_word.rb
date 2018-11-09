9e8c75e09f534d591b5360470c111e3d1701c8c9
Angie Goza <angielg9888@gmail.com>
2018-11-09 04:50:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,7 @@ def reverse_each_word(sentence)
   string_to_array << sentence.split
   new_sentence = [ ]
   string_to_array.each do |word|
-    new_word = word.split
-    new_sentence << new_word.reverse
+    new_sentence << word.reverse
   end
   return new_sentence
 end
