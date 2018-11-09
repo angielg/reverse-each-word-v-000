686ed6fc33469bd3478f21d0b21011bb93e8235f
Angie Goza <angielg9888@gmail.com>
2018-11-09 04:33:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentence)
-  sentence.split
+  new_sentence = [ ]
+  sentence.split = new_array
+  new_array.each do |word|
+    new_sentence << word.reverse
+  end
+  return new_sentence
 end
